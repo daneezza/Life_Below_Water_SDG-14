041aeebc29f7e0a2e902da80cae6e34dcba080d8
Daneesha Hansaka <daneesha.2003@hotmail.com>
2024-07-12 07:27:50 +0530

    Update splash.js

--- splash.js
+++ splash.js
@@ -1,7 +1,6 @@
 var splashScreen = document.querySelector('.splash');
 
 setTimeout(function() {
-    splashScreen.classList.add('fade-out');
     setTimeout(function() {
         window.location.href = 'home.html';
     }, 500);
